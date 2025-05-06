61e5df92e3c12a4105a39cfb033a3be946b3c84c
Aiden Kramer <kramer.aiden.a@gmail.com>
2025-05-06 08:41:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+pip3 install tkinter
+pip3 install pyperclip
+pip3 install os
+pip3 install subprocess
+pip3 install platform
+echo 'If you have not changed the paths on line 8 yet, do it now!'
+cat /path/to/pythonpad.txt > ~/.local/share/applications/pythonpad.desktop
+echo 'Install complete!'
+sudo restart
